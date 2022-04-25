b514a2974e952131f0ee546674cd6b2ff92febc2
josh <holtz185@umn.edu>
2022-04-24 19:44:06 -0500

    Working Etch-a-sketch with binary (black/white) trace

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
 
 function initializeGrid(rows, columns){
     const container = document.createElement('div');
-    container.setAttribute('style', 'display: flex; flex-direction: column; margin: 0; width: 100%; height: 100%; border: 2px solid green;');
+    container.setAttribute('style', 'display: flex; flex-direction: column; margin: 0; width: 100%; height: 100%;');
     //style editing here
 
     for (let i = 0; i < rows; i++){
         let containerRow = document.createElement('div');
-        containerRow.style.cssText = `display: flex; margin: 0; width: 100%; height: 100%; border: 2px solid blue;`;
+        containerRow.style.cssText = `display: flex; margin: 0; width: 100%; height: 100%;`;
         for (let j = 0; j < columns; j++){
             let containerCol = document.createElement('div');
-            containerCol.style.cssText = `margin: 0; border: 2px solid red; height: 100%; width: 100%;`;
+            containerCol.style.cssText = `margin: 0; height: 100%; width: 100%;`;
+            containerCol.className = "grid-cell";
             containerRow.appendChild(containerCol);
         }
         container.appendChild(containerRow);
     }
     document.body.appendChild(container);
+
+    color();
 }
 
-initializeGrid(50, 50);
+
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    let size = prompt('How big of a square grid would you like? (max of 100)');
+    while (size > 100){
+        size = prompt('Invalid entry. Please enter an integer between 1 and 100');
+    }
+    let container = document.querySelector("body > div");
+    container.parentNode.removeChild(container);
+    initializeGrid(size, size);
+});
+
+
+initializeGrid(20, 20);
+
+
+function color(){
+    const cells = document.querySelectorAll('.grid-cell');
+    cells.forEach(cell => cell.addEventListener('mouseover', e => {
+        let cell = e.target;
+        cell.classList.add('grid-cell-visited');
+    }));
+}
